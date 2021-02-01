a469b63e04032dd2a2fddaef4b59bdc8f4108fd7
Soroush Famili <soroushfamili99@gmail.com>
2021-02-01 11:16:36 -0600

    Initial commit. Oncatenator changed to work with Amazon data. Model created but isn't accurate. Too little data?

---
+++ model/Concatenator.py
@@ -0,0 +1,126 @@
+import os
+import numpy as np
+import torch
+import torchvision.transforms as T
+from torch.utils.data import Dataset
+from PIL import Image
+import pandas as pd
+
+class Concatenator(Dataset):
+
+	def __init__(self, parent_dir, csvfile, image_dim=(600, 600)):
+		self.concatenated_images = []
+		self.first_images = []
+		self.second_images = []
+		self.labels = []
+
+		self.transform = T.Compose([
+			T.ToPILImage(),
+			T.Resize(image_dim),
+			T.ToTensor()])
+
+		self.load(parent_dir, csvfile)
+
+	def first_images(self):
+		return self.first_images
+
+	def second_images(self):
+		return self.second_images
+
+	def concatenated_images(self):
+		return self.concatenated_images
+
+	def __len__(self):
+		return len(self.concatenated_images)
+
+	def __getitem__(self, index):
+		item = self.concatenated_images[index]
+		label = self.labels[index]
+		return item, label
+
+	def create_folder(self, path):
+		if not os.path.isdir(path):
+			try:
+				os.makedirs(path)
+				print(f"Created Folder @ {path}")
+
+			except OSError as error:
+				print("Folder cannot be created")
+				print(error)
+		else:
+			print(f"Folder @ {path} Already Exists")
+			for file in os.listdir(path):
+				os.remove(os.path.join(path, file))
+
+	def concat_name(self, dir_path, name1, name2):
+		name = name1 + '+' + name2
+		concat_path = os.path.join(dir_path, name)
+
+		return concat_path
+
+	def rand_order(self, name1, name2):
+		flag = False
+		if np.random.binomial(1, .5):
+			temp = name1
+			name1 = name2
+			name2 = temp
+			flag = True
+
+		return flag, name1, name2
+
+
+	def load(self, parent_dir, csvfile):
+
+		first_dataset = "Benign"
+		second_dataset = "Manipulated"
+
+		path1 = os.path.join(parent_dir, first_dataset)
+		path2 = os.path.join(parent_dir, second_dataset)
+
+
+		new_dir = str(first_dataset + '+' + second_dataset)
+		path3 = os.path.join(parent_dir, new_dir)
+		self.create_folder(path3)
+
+		csvpath = os.path.join(parent_dir, csvfile)
+
+		df = pd.read_csv(csvpath)
+
+		for i in df.index:
+
+			name1 = df["image1"][i]
+			image1 = np.asarray(Image.open(path1 + '/' + name1))
+
+			name2 = df["image2"][i]
+
+			if name2[0] == 'b':
+				image2 = np.asarray(Image.open(path1 + '/' + name2))
+			else:
+				image2 = np.asarray(Image.open(path2 + '/' + name2))
+
+			self.first_images.append(image1)
+			self.second_images.append(image2)
+
+			if df["label"][i] == "same":
+				self.labels.append(1)
+			else:
+				self.labels.append(0)
+
+			flag, name1, name2 = self.rand_order(name1, name2)
+
+			concat_name = self.concat_name(path3, name1[:-4], name2[:-4])
+			if not os.path.isdir(concat_name):
+				transformed1 = self.transform(image1)
+				transformed2 = self.transform(image2)
+
+				if flag:
+					concatenated = torch.cat((transformed2, transformed1), 0)
+				else:
+					concatenated = torch.cat((transformed1, transformed2), 0)
+
+
+				self.concatenated_images.append(concatenated)
+				np.save(concat_name, concatenated)
+
+			else:
+				self.concatenated_image.append(np.load(concat_name))
---
+++ model/binary_classifier.py
@@ -0,0 +1,72 @@
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+from modified_alexnet import Modified_AlexNet
+from Concatenator import Concatenator
+from pathlib import Path
+
+curr_path = os.path.dirname(os.path.abspath(__file__))
+datasets_path = str(Path(curr_path).parents[0]) +  "/AmazonSet"
+
+print("Concatinating training data")
+training_concatenator = Concatenator(datasets_path, "train.csv")
+print("Finished")
+
+print("Concatinating testing data")
+testing_concatenator = Concatenator(datasets_path, "test.csv")
+print("Finished")
+
+print("Loading training set")
+batch_size = 64
+trainloader = DataLoader(training_concatenator, batch_size, shuffle=True)
+
+print("Loading testing set")
+batch_size = 64
+testloader = DataLoader(testing_concatenator, batch_size, shuffle=False)
+
+print("Creating Model")
+model = Modified_AlexNet()
+
+print("Training Model")
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):
+
+	running_loss = 0.0
+	for i, batch in enumerate(trainloader, 0):
+
+		concat_images, labels = batch
+		optimizer.zero_grad()
+
+		outputs = model(concat_images)
+		loss = criterion(outputs, labels)
+		loss.backward()
+		optimizer.step()
+
+		running_loss += loss.item()
+
+	print(f"Epoch {epoch}: Loss = {running_loss}")
+	running_loss = 0
+
+print("Testing Model")
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+	for data in testloader:
+		images, labels = data
+		outputs = model(images)
+		_, predicted = torch.max(outputs.data, 1)
+		total += labels.size(0)
+		correct += (predicted == labels).sum().item()
+
+print(f"Accuracy over test set: {100*correct/total}%")
+print(correct)
+print(total)
+
