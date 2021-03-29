4ad5c9da47386bbe32d280092dd6aff176cee753
Soroush Famili <soroushfamili99@gmail.com>
2021-03-29 00:25:24 -0500

    Combined all folder manipulation into sort.py, Created ClassSplitter which limits the number of classes to train on.

--- DataSets/moveup.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def moveup(path):
-	cwd = path
-
-	for folder in os.listdir(cwd):
-		if os.path.isdir(os.path.join(cwd, folder)):
-			for child in os.listdir(os.path.join(cwd, folder)):
-				os.rename(os.path.join(os.path.join(cwd, folder), child), os.path.join(cwd, child))
