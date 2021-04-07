82ce5b773caec2ce9491711ee8eeb1e9ac6f2024
Soroush Famili <soroushfamili99@gmail.com>
2021-04-07 10:27:19 -0500

    Updated binary_classifier

--- model/binary_classify.py
+++ model/binary_classify.py
@@ -19,25 +19,24 @@ def classify(image1, image2, size='small'):
 
     model = Modified_LeNet(batch_size=batch_size, dim=dim)
 
-    # Commented this out because it was causing errors, but this seems to hurt accuracy
-    # parameters_path = os.path.join(os.getcwd(), "parameters")
-    # model.load_state_dict(torch.load(
-    #     os.path.join(parameters_path, size + '.pth')))
+    parameters_path = os.path.join(os.getcwd(), "parameters")
+    model.load_state_dict(torch.load(
+    os.path.join(parameters_path, size + '.pth')))
 
     output = model(concatenated_image)
     _, predicted = torch.max(output.data, 1)
     return predicted.numpy().tolist()[0]
 
 
+#Example code
 """
-Example code
 image1 = '/c/Users/sorou/Desktop/SeniorDesign/Datasets/Wallets/B000078QZN/B000078QZN_0.jpg'
 image2 = '/c/Users/sorou/Desktop/SeniorDesign/Datasets/Wallets/B000078QZN/B000078QZN_4.jpg'
 
-print(classify(image1, image2, size='small'))
+print(classify(image1, image2, size='large'))
 """
 
 # image1 = '.\DataSets\Accessories\B000922SGS_9.jpg'
 # image2 = '.\DataSets\Accessories\B000922SGS_1.jpg'
 
-# print(classify(image1, image2, size='small'))
+# print(classify(image1, image2, size='large'))
