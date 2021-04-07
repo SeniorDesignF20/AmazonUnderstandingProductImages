42412027c5851da44099f04a595a2ee6840d1f9c
Matt MacDonald <mattkmacd@gmail.com>
2021-04-06 23:22:45 -0500

    Classify attached to UI
    Renamed binary_classifier.py in models to binary_classify.py
    Parameter lines in classify commented out.

--- UserInterface/binary_classifier/views.py
+++ UserInterface/binary_classifier/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 import os
+from pathlib import Path
+import sys
 from binary_classifier.storage import OverwriteStorage
 
 # views.py
 
+
 def binary_classifier(request):
     img1_path = ''
     img2_path = ''
+    classify_val = 'NaN'
 
     if request.method == 'POST' and 'image' in request.FILES:
         img = request.FILES['image']
@@ -24,12 +28,21 @@ def binary_classifier(request):
         return redirect('binary_classifier')
 
     if os.path.isfile('./media/image1.jpg'):
-            img1_path = './media/image1.jpg'
+        img1_path = './media/image1.jpg'
     if os.path.isfile('./media/image2.jpg'):
-            img2_path = './media/image2.jpg'
+        img2_path = './media/image2.jpg'
 
-    return render(request, 'binary_classifier.html', {'img1_path': img1_path, 'img2_path': img2_path})
+    if request.method == 'POST' and 'classify' in request.POST:
+        curr_path = os.path.dirname(os.path.abspath(__file__))
+        classify_path = str(
+            Path(curr_path).parents[1]) + '\model'
+        sys.path.insert(1, classify_path)
+        print(classify_path)
+        from binary_classify import classify
+        classify_val = classify(img1_path, img2_path, "small")
+
+    return render(request, 'binary_classifier.html', {'img1_path': img1_path, 'img2_path': img2_path, 'classify_val': classify_val})
+
 
 def about(request):
     return render(request, 'about.html')
-
---
+++ model/binary_classify.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import torch
+from modified_lenet import Modified_LeNet
+from Concatenator import Concatenator
+
+# image1 and image2 are expected to be paths to the images
+
+
+def classify(image1, image2, size='small'):
+    concatenator = Concatenator()
+    concatenated_image = concatenator.concatenate(image1, image2)
+    concatenated_image = torch.tensor(
+        np.expand_dims(concatenated_image, axis=0))
+    batch_size = 64
+    image_dim = (56, 56)
+    dim = image_dim[0]
+    dim = int((int(dim/2) - 2)/2) - 6
+
+    model = Modified_LeNet(batch_size=batch_size, dim=dim)
+
+    # Commented this out because it was causing errors, but this seems to hurt accuracy
+    # parameters_path = os.path.join(os.getcwd(), "parameters")
+    # model.load_state_dict(torch.load(
+    #     os.path.join(parameters_path, size + '.pth')))
+
+    output = model(concatenated_image)
+    _, predicted = torch.max(output.data, 1)
+    return predicted.numpy().tolist()[0]
+
+
+"""
+Example code
+image1 = '/c/Users/sorou/Desktop/SeniorDesign/Datasets/Wallets/B000078QZN/B000078QZN_0.jpg'
+image2 = '/c/Users/sorou/Desktop/SeniorDesign/Datasets/Wallets/B000078QZN/B000078QZN_4.jpg'
+
+print(classify(image1, image2, size='small'))
+"""
+
+# image1 = '.\DataSets\Accessories\B000922SGS_9.jpg'
+# image2 = '.\DataSets\Accessories\B000922SGS_1.jpg'
+
+# print(classify(image1, image2, size='small'))
