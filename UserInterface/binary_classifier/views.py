6bd3f2c8a11265822963591894c4ee802034f8c1
Jessica Pham <jessica.pham@utexas.edu>
2021-04-06 19:41:55 -0500

    update to show preview of uploaded image(s)

--- UserInterface/binary_classifier/views.py
+++ UserInterface/binary_classifier/views.py
@@ -2,20 +2,33 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+import os
+from binary_classifier.storage import OverwriteStorage
 
 # views.py
 
 def binary_classifier(request):
-    if request.method == 'POST':
+    img1_path = ''
+    img2_path = ''
+
+    if request.method == 'POST' and 'image' in request.FILES:
         img = request.FILES['image']
-        fs = FileSystemStorage()
+        fs = OverwriteStorage()
+
+        # check which upload button was clicked
         if 'image1_btn' in request.POST:
             filename = fs.save('image1.jpg', img)
-            return render(request, 'binary_classifier.html', {'upload1_successful': True})
-        if 'image2_btn' in request.POST:
+        elif 'image2_btn' in request.POST:
             filename = fs.save('image2.jpg', img)
-            return render(request, 'binary_classifier.html', {'upload2_successful': True})
-    return render(request, 'binary_classifier.html')
+
+        return redirect('binary_classifier')
+
+    if os.path.isfile('./media/image1.jpg'):
+            img1_path = './media/image1.jpg'
+    if os.path.isfile('./media/image2.jpg'):
+            img2_path = './media/image2.jpg'
+
+    return render(request, 'binary_classifier.html', {'img1_path': img1_path, 'img2_path': img2_path})
 
 def about(request):
     return render(request, 'about.html')
